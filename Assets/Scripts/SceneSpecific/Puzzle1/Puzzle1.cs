4ad259c70c262164aeea8801dabe640f9741d6d6
abner <abnermtj@live.com.sg>
2023-04-25 16:37:30 +0800

    Update starting convo puzzle 1

--- Assets/Scripts/SceneSpecific/Puzzle1/Puzzle1.cs
+++ Assets/Scripts/SceneSpecific/Puzzle1/Puzzle1.cs
@@ -1,12 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Chronellium.EventSystem;
+using UnityEngine.SceneManagement;
 
 public class Puzzle1 : MonoBehaviour
 {
-    public Conversation startingConvo;
     private void Start()
     {
-        DialogueManager.Instance.StartConversation(startingConvo);
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetPuzzle();
+        }
+        EventManager.StartListening(StaticEvent.Core_ResetPuzzle, ResetPuzzle);
+    }
+
+    private void ResetPuzzle(object input = null)
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
     }
 }
